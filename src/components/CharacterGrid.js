57b55d3c9c86cb30170cf345afe826ec935c59d1
Angel Cruz <angi_ed01@hotmail.com>
2020-09-10 14:31:57 -0500

    CharacterItem creado

--- src/components/CharacterGrid.js
+++ src/components/CharacterGrid.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CharacterItem } from './CharacterItem'
 
 export const CharacterGrid = ({ characters }) => {
 
@@ -6,24 +7,7 @@ export const CharacterGrid = ({ characters }) => {
         <div className="row row-cols-1 row-cols-md-2">
             {
                 characters.map(cha => (
-                    <div className="card mb-3" key={cha.id}>
-                        <div className="row no-gutters">
-                            <div className="col-md-4">
-                                <img src={cha.image} className="card-img" alt="Rick and Morty"/>
-                            </div>
-                            <div className="col-md-8 textCards">
-                                <div className="card-body">
-                                    <h5 className="card-title">{cha.name}</h5>
-                                    <p className="card-text">
-                                        <b>Status:</b> {cha.status}  <br />
-                                        <b>Specie:</b> {cha.species}  <br />
-                                        <b>Origin:</b> {cha.origin}  <br />
-                                        <b>Location:</b> {cha.location}  <br />
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                   <CharacterItem cha= {cha}/>
                 ))
             }
         </div>
